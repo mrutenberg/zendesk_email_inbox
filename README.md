c575faaaa278b229fb4c13e317fddffb40db2ef8
mrutenberg <mark@mrut.me>
2016-04-20 20:52:05 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Email Inbox
+
+This app creates a outlook-esque email inbox for all incomming email tickets.
---
+++ app.js
@@ -0,0 +1,12 @@
+(function() {
+
+  return {
+
+    storage: {},
+
+    events: {
+      
+    },
+
+  };
+}());
